21e521d4a8f163c1f7567411625d135093003893
Deconx <deconx@vip.qq.com>
2022-10-24 22:49:14 +0800

    init

---
+++ scripts/run_example.sh
@@ -0,0 +1,27 @@
+TRAIN_FILE_PATH=./data/zh_ar.train
+# EVAL_FILE_PATH=/your/path/to/eval.txt
+OUTPUT_DIR=../checkpoints/
+RUN_PATH=../runs/
+MODEL_PATH=../checkpoints/zh_ar/
+# python main.py \
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 --master_port 29501 main.py \
+    --dataset_name zh_ar \
+    --src_pretrain_dataset_name "bert-base-chinese" \
+    --tgt_pretrain_dataset_name "aubmindlab/aragpt2-base" \
+    --train_data_path $TRAIN_FILE_PATH \
+    --output_dir $OUTPUT_DIR \
+    --run_path $RUN_PATH \
+    --finetune_lr 3e-5 \
+    --lr 1e-4 \
+    --num_training_steps 150000 \
+    --num_warmup_steps 10000 \
+    --max_src_len 128 \
+    --max_tgt_len 128 \
+    --save_step 1 \
+    --nepoch 3 \
+    --ngpu 1 \
+    --train_batch_size 32 \
+    --eval_batch_size 32 \
+    # --drop_last \
+    # --model_path $MODEL_PATH \
+    # --ispredict \
